320c35291bf08313ee2f06fa0f859055efc53df1
张正华 (WF/ITA) <zhenghua.zhang@weifu.com.cn>
2017-09-30 14:28:16 +0800

    非ms，包装后直接入库

--- src/com/ptsoft/controller/api/PkgController.java
+++ src/com/ptsoft/controller/api/PkgController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ptsoft.pts.basic.service.TraceService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,6 +36,8 @@ public class PkgController {
 	private UserService userService;
 	@Autowired
 	private PackageRuleService packageRuleService;
+	@Autowired
+	private TraceService traceService;
 	
 	Logger logger = Logger.getLogger(PkgController.class);
 
@@ -88,13 +91,23 @@ public class PkgController {
 	@RequestMapping(value="pkgRecord", method=RequestMethod.POST)
 	public void savePkgRecord(HttpServletRequest request, HttpServletResponse response, String outerCode, String innerCodes, int type, String deviceNo)
 	{
+		// 测试数据
+//		int type = 2;
+//		String deviceNo = "001";
+//		String outerCode = "2017-09-30-442f5187-bf5d-49b4-8ce4-98842193e0bb";
+//		String innerCodes = "2017-09-30-c20fa7ee-02be-4f9c-9abc-d689cf778580,2017-09-30-b447f47a-a1c1-4a77-b12b-450be29ddca4,2017-09-30-c486673e-fcb2-46b2-b5fd-c24a4099ab00,2017-09-30-e00d99b0-a52d-441f-990f-5f27d2c5b3bc,2017-09-30-48ab067f-c8d7-459a-b156-5f430637cc5d,2017-09-30-4640374c-b736-46f8-998b-745fa5fd1fc8,2017-09-30-2b349e6f-255a-448d-b3a4-c964353d0831,2017-09-30-9b10e521-29ae-4f69-8ee2-f8f98adf156b,2017-09-30-6a2d3687-f4cf-4f49-b035-1ada92d487f3,2017-09-30-d5a88447-2c30-433f-9a75-6d0169a12d11";
+
+		boolean isPackaged = false;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		HashMap<String, Object> map = new HashMap<String, Object>();
+		SysUser user = null;
 		try 
 		{
 			String username = request.getParameter("username");
 			String token = request.getParameter("token");
-			SysUser user = userService.findByNameAndToken(username, token);
+//			String username = "zhangzh";
+//			String token = "84302c00-2cf2-492a-83d1-d7868b1ac59b";
+			user = userService.findByNameAndToken(username, token);
 			if(null == user)
 			{
 				map.put("code", 0);
@@ -104,6 +117,9 @@ public class PkgController {
 			{
 				map = pkgService.savePkgRecord(outerCode, innerCodes, type, user, deviceNo);
 			}
+			if ((Integer)map.get("code") == 1) {
+				isPackaged = true;
+			}
 		} 
 		catch (Exception e) 
 		{
@@ -112,8 +128,18 @@ public class PkgController {
 			map.put("msg", "包装失败，出现异常");
 			map.put("msg_en", "Package failure. There are some exceptions.");
 			map.put("msg_code", 1002);
+			isPackaged = false;
 		}
 		ResponseUtils.renderJson(response, map);
+
+		// 包装成功后入库
+		if (isPackaged) {
+			try {
+				traceService.saveIn(user, outerCode, deviceNo);
+			} catch (Exception e) {
+				logger.error("------包装后自动入库异常------" + e.toString());
+			}
+		}
 	}
 	
 }
